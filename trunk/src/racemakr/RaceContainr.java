9ba2cf524850f98c408a3004588c5f88642c82b9
jacobs.mh <jacobs.mh@aee99aee-7a48-0410-bd0a-d9df811b1ca7>
2008-03-22 22:25:50 +0000

    Adding RaceContainr class

---
+++ trunk/src/racemakr/RaceContainr.java
@@ -0,0 +1,13 @@
+package racemakr;
+
+import java.util.Collection;
+
+import processing.core.PImage;
+
+public class RaceContainr {
+
+	PImage _cropped, _original;
+	Collection<String> _sentences;
+	RaceMakr.Race _race;
+	String _label;
+}
